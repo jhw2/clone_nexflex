8b7bae6d38d12813a8b951348443e799f41ee86a
jhw <hope5107@naver.com>
2022-09-13 22:56:13 +0900

    메임페이지, 검색페이지, 상세페이지 추가

---
+++ src/pages/Mainpage/index.tsx
@@ -0,0 +1,30 @@
+import { requests } from "../../api/request";
+import "../../App.css";
+import { Banner } from "../../components/Banner";
+import { Footer } from "../../components/Footer";
+import { Nav } from "../../components/Nav";
+import { Row } from "../../components/Row";
+
+export const MainPage = () => {
+  return (
+    <div>
+      <Banner />
+      <Row
+        title="NEXFLIX ORIGINALS"
+        id="NO"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+      />
+      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
+      <Row
+        title="Action Movies"
+        id="AM"
+        fetchUrl={requests.fetchActionMovies}
+      />
+      <Row title="Comedy Movie" id="CM" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movie" id="HM" fetchUrl={requests.fetchHorroryMovies} />
+      <Row title="Romance Movie" id="RM" fetchUrl={requests.fetchRomanceMovies} />
+    </div>
+  );
+}
---
+++ src/pages/searchPage/index.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import axios from "../../api/axios";
+import { useAxios } from "../../hooks/useAxios";
+import { Error } from "../../components/share/Error";
+import "./SearchPage.css";
+import { IMovidetail, IMovieLists_Response } from "../../Types/MovieTypes";
+
+export const SearchPage = () => {
+  const navigate = useNavigate();
+  const useQuery = () => new URLSearchParams(useLocation().search);
+  let query = useQuery();
+  const searchTerm = query.get("q");
+  const {
+    isloading,
+    data,
+    errorMsg,
+  } = useAxios<IMovieLists_Response, null>(
+    `/search/multi?include_adult=false&query=${searchTerm}`,
+    {},
+    axios
+  );
+  const searchResults = data?.results;
+  console.log(searchResults);
+  return errorMsg ? (
+    <Error msg={errorMsg} />
+  ) : searchResults && searchResults?.length > 0 ? (
+    <section className="search-container">
+      {searchResults?.map((movie) => {
+        if (movie.backdrop_path !== null && movie.media_type !== "person") {
+          const movieImageUrl =
+            "https://image.tmdb.org/t/p/w500" + movie.backdrop_path;
+          return (
+            <div className="movie" key={movie.id}>
+              <div
+                onClick={() => navigate(`/${movie.id}`)}
+                className="movie__column-poster"
+              >
+                <img
+                  src={movieImageUrl}
+                  alt="movie"
+                  className="movie__poster"
+                />
+              </div>
+            </div>
+          );
+        }
+      })}
+    </section>
+  ) : (
+    <section className="no-results">
+      <div className="no-results__text">
+        <p>찾고자하는 검색어에 맞는 영화가 없습니다.</p>
+      </div>
+    </section>
+  );
+};
