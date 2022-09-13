b667edd09db2b3797f03e548c2da118681b40907
jhw <hope5107@naver.com>
2022-09-13 22:53:00 +0900

    react-router-dom 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,34 @@
+import { Outlet, Route, Routes } from "react-router-dom";
 import { requests } from "./api/request";
 import "./App.css";
 import { Banner } from "./components/Banner";
 import { Footer } from "./components/Footer";
 import { Nav } from "./components/Nav";
 import { Row } from "./components/Row";
+import { DetailPage } from "./pages/DetailPage";
+import { MainPage } from "./pages/Mainpage";
+import { SearchPage } from "./pages/searchPage";
+
+const Layout = () => {
+  return (
+    <div>
+      <Nav />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <Banner />
-      <Row
-        title="NEXFLIX ORIGINALS"
-        id="NO"
-        fetchUrl={requests.fetchNetflixOriginals}
-        isLargeRow={true}
-      />
-      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
-      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
-      <Row
-        title="Action Movies"
-        id="AM"
-        fetchUrl={requests.fetchActionMovies}
-      />
-      <Row title="Comedy Movie" id="CM" fetchUrl={requests.fetchComedyMovies} />
-      <Row title="Horror Movie" id="HM" fetchUrl={requests.fetchHorroryMovies} />
-      <Row title="Romance Movie" id="RM" fetchUrl={requests.fetchRomanceMovies} />
-      <Footer />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />}></Route>
+          <Route path=":movieId" element={<DetailPage />}></Route>
+          <Route path="search" element={<SearchPage />}></Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
