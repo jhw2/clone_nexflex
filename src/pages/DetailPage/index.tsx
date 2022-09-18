3d7638afe151d48ca7b551dcb3cffc27ef59eefb
jhw <hope5107@naver.com>
2022-09-18 21:46:10 +0900

    영화 상세페이지 추가

--- src/pages/DetailPage/index.tsx
+++ src/pages/DetailPage/index.tsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
+import axios from "../../api/axios";
+import { useAxios } from "../../hooks/useAxios";
+import { IMovidetail } from "../../Types/MovieTypes";
+import { Image } from "../../components/sections/Image";
 
 export const DetailPage = () => {
-  return (
-    <div>index</div>
-  )
-}
+  const { movieId } = useParams();
+
+  const { isOk, data: movie } = useAxios<IMovidetail, null>(
+    `movie/${movieId}`,
+    {},
+    axios
+  );
+  return !movie ? (
+    <div></div>
+  ) : (
+    <section>
+      <Image
+        src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+        clss={"movie__poster"}
+        alt={movie.title ?? movie.name}
+      />
+    </section>
+  );
+};
